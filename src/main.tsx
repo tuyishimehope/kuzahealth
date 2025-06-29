9c4e9765546cfb9c4a3c02275ed1ed113fbd202f
MAC <MAC@KyrieM1pro.local>
2025-06-29 19:30:02 +0200

    fix errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/route.tsx'
+import { router } from './routes/route'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
